1530de711a40a342ff9edcd63a75b724a7a685ed
Xavier Francis <james.x.francis9@gmail.com>
2023-04-20 16:18:20 -0500

    first vue exercise complete

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+
+<script setup>
+let user = "Xavier";
+</script>
+
+<template>
+  <h1>Hello, {{user}}!</h1>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
